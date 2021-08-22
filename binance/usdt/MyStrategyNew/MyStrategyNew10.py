19a8143360c2c50b8f62eddedb54a2031aea7bcd
Jirawat Boonkumnerd <jo06942@gmail.com>
2021-08-23 05:13:06 +0700

    fix: disable sell_profit_only

--- binance/usdt/MyStrategyNew/MyStrategyNew10.py
+++ binance/usdt/MyStrategyNew/MyStrategyNew10.py
@@ -161,11 +161,11 @@ class MyStrategyNew10(IStrategy):
 
     # These values can be overridden in the "ask_strategy" section in the config.
     use_sell_signal = True
-    sell_profit_only = True
+    sell_profit_only = False
     ignore_roi_if_buy_signal = True
 
     # Number of candles the strategy requires before producing valid signals
-    startup_candle_count: int = 500
+    startup_candle_count = 500
 
     def __init__(self, config: dict) -> None:
         super().__init__(config)
